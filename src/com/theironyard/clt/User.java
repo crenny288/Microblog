aa44edbc04278c1cf22f7b4c9af29b6bbb371437
crenny288 <crenny288@gmail.com>
2016-04-13 17:13:26 -0400

    microblog homework, still working

---
+++ src/com/theironyard/clt/User.java
@@ -0,0 +1,36 @@
+package com.theironyard.clt;
+
+import sun.misc.resources.Messages;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static com.sun.org.apache.xml.internal.serializer.utils.Utils.messages;
+import static com.theironyard.clt.Main.user;
+
+/**
+ * Created by mac on 4/13/16.
+ */
+public class User {
+    //write code here
+    public String name;
+
+    public ArrayList<String>  messages;
+
+    public User(String name) {
+        messages = new ArrayList<>();
+    }
+    
+
+
+//    public User(String name) {
+//
+//        this.name = name;
+//    }
+
+
+
+    }
+
+
+}
